66b16e5a862efe1926247a0e0b44c1103f01a484
Do Trong Anh <d9dtanh@cmcglobal.vn>
2022-07-12 16:58:41 +0700

    update

--- sql_app/database.py
+++ sql_app/database.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # just need to declare db name like below, and run the app, it will auto create db for you
+# because in main.py, there's a line to run migrate db
+# search for this line in main.py: models.Base.metadata.create_all(bind=engine)
 
 # sqlite db
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
